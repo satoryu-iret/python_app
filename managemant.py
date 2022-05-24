3954bd5b6fc40d6ec41827216aa993755c05577e
masaki <masaki@cloudpack.jp>
2022-05-24 14:45:44 +0900

    Revert "編集中"
    This reverts commit 1c9d3d5c866445babe50fcec36bbdff39a6d18d7.

--- managemant.py
+++ managemant.py
@@ -35,17 +35,17 @@ class Management:
             sql = "SELECT menu.menu_id, menu.menu_name, menu_sales.sales FROM menu INNER JOIN menu_sales ON menu.menu_id = menu_sales.menu_id;"
             rows = cursor.execute(sql)
             rows = cursor.fetchall()
-            # print('売り上げを更新します')
-            # for row in rows:
-            #     print(row[1], 'の現在の売り上げ: ', row[2])
-            #     print('↓↓↓↓↓今回の売り上げを入力↓↓↓↓↓')
-            #     num = int(input())
-            #     if type(num) == int:
-            #         earning = int(row[2]) + int(num)
-            #         sql = "UPDATE menu_sales SET sales = %s WHERE menu_id = %s"
-            #         cursor.execute(sql, (earning, row[0]))
-            #         connection.commit()
-            #         print('更新後の売り上げ: ', earning)
+            print('売り上げを更新します')
+            for row in rows:
+                print(row[1], 'の現在の売り上げ: ', row[2])
+                print('↓↓↓↓↓今回の売り上げを入力↓↓↓↓↓')
+                num = int(input())
+                if type(num) == int:
+                    earning = int(row[2]) + int(num)
+                    sql = "UPDATE menu_sales SET sales = %s WHERE menu_id = %s"
+                    cursor.execute(sql, (earning, row[0]))
+                    connection.commit()
+                    print('更新後の売り上げ: ', earning)
                     # while True:
                     #     updateConfirm = input('確定したい場合は y 修正したい場合は n を押してください: ')
                     #     if updateConfirm == 'y':
@@ -56,31 +56,10 @@ class Management:
                     #         print('???????????????????????')
                     #     else:
                     #         continue
-                # else:
-                #     print('数字で入力してください')
-                #     break  
-                
-                updateData = {}
-                print('売り上げを更新します')
-                for row in rows:
-                    print(row[1], 'の現在の売り上げ: ', row[2])
-                    print('↓↓↓↓↓今回の売り上げを入力↓↓↓↓↓')
-                    num = int(input())
-                    if type(num) == int:
-                        earning = int(row[2]) + int(num)
-                        updateData += {
-                            row[1]: earning
-                        }
-                        # sql = "UPDATE menu_sales SET sales = %s WHERE menu_id = %s"
-                        # cursor.execute(sql, (earning, row[0]))
-                        # connection.commit()
-                        print('更新後の売り上げ: ', earning)
-                    else:
+                else:
                     print('数字で入力してください')
                     break  
-                print(updateData)
-
-
+                
 
         # 例外処理ブロック
         except Exception as e:
